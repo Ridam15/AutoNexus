1a2540da07c9a907090bf0993e00baee29da021a
RidamChhapiya <ridam.chhapiya@searce.com>
2025-08-01 16:23:37 +0530

    updates

--- src/automax/settings.py
+++ src/automax/settings.py
@@ -31,7 +31,7 @@ SECRET_KEY = env('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = bool(env('DEBUG'))
 
-ALLOWED_HOSTS = ['autonexus-igro.onrender.com']
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
